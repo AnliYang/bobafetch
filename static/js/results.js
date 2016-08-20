634825f3abcd45e43331df5a43577b303cbbe40b
AnliYang <anli.m.yang@gmail.com>
2016-08-20 03:28:52 -0700

    Clean up results JS

--- static/js/results.js
+++ static/js/results.js
@@ -1,14 +1,11 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiYW5saXlhbmciLCJhIjoiY2lvZ25wbTB4MDFrdHU3a212eGZwcW91NSJ9.GOtW72gefCHdD1Y-6bza-w';
 
-// var restaurantIds = $('#map_geo').data('restaurant-ids');
-// $.get('/restaurant-locations.geojson', {'restaurant-ids': restaurantIds}, addMarkers);
-
 var userLat = $('#map_geo').data('user-lat');
 var userLng = $('#map_geo').data('user-lng');
 var features = $('#map_geo').data('feature-collection')
 
 var mapGeo = L.mapbox.map('map_geo', 'mapbox.dark')
-  .setView([userLat, userLng], 14);
+  .setView([userLat, userLng], 15);
 
 function addMarkers(features) {
     L.mapbox.featureLayer().setGeoJSON(features).addTo(mapGeo);
@@ -16,16 +13,3 @@ function addMarkers(features) {
 }
 
 addMarkers(features);
-
-
-// function addMarkers(results) {
-//     L.mapbox.featureLayer().setGeoJSON(results).addTo(mapGeo);
-//     mapGeo.scrollWheelZoom.disable();
-// }
-
-// var resultsElement = document.getElementById('results');
-//
-// var restaurants = resultsElement.dataset.restaurants;
-// var range = resultsElement.dataset.range;
-//
-// var restaurantIds = $('#map_geo').data('restaurant-ids');
